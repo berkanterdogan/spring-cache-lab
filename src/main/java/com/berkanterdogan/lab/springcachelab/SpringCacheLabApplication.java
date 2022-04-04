6aa9a72796f451ed235c5f8451ab5b739e84a9f1
berdogan <berkanterdogan92@gmail.com>
2022-04-04 23:24:21 +0300

    first commit. The project was initialized

---
+++ src/main/java/com/berkanterdogan/lab/springcachelab/SpringCacheLabApplication.java
@@ -0,0 +1,13 @@
+package com.berkanterdogan.lab.springcachelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheLabApplication.class, args);
+    }
+
+}
